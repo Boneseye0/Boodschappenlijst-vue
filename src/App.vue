22c9bf3fc4676ed28fb3c7023b55f86e663cdc58
Boneseye0 <svrosevelt@gmail.com>
2026-03-12 13:20:33 +0100

    task complete
    potential to do - css formatting

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,61 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
+import { computed, ref } from 'vue';
+
+const groceries = ref([
+  {
+  product     :   'Rijst',
+  prijs       :   1.00,
+  aantal      :   1
+},
+  {
+  product     :   'Broccoli',
+  prijs       :   0.99,
+  aantal      :   1
+},
+  {
+  product     :   'Koekjes',
+  prijs       :   1.20,
+  aantal      :   1
+},  
+  {
+  product     :   'Noten',
+  prijs       :   2.99,
+  aantal      :   1
+}])
+
+const computedSubtotaal = computed(() => { 
+  return groceries.value.map(item => item.prijs * item.aantal)
+})
+
+const computedTotal = computed(() => {
+ return computedSubtotaal.value.reduce((total, num) => total + num , 0)
+})
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<div>
+  <table>
+    <thead>
+      <tr>
+        <td>Product</td><td>prijs</td><td>aantal</td><td>subtotaal</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(item, index) in groceries" :key="index">
+        <td>{{ item.product }}</td><td class="money">€{{ item.prijs.toFixed(2) }}</td><td><input type="number" v-model="item.aantal" /></td><td class="money">€{{ computedSubtotaal[index].toFixed(2) }}</td>
+      </tr>
+    </tbody>
+    <tfoot>
+      <tr>
+        <td colspan="3">totaal</td><td class="money">€{{ computedTotal.toFixed(2) }}</td>
+      </tr>
+    </tfoot>
+  </table>
+</div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
